ab9ee6312cc7bbd9223728fa7db1f7fa85ec1f40
patrick <patrick.mcisaac.dev@gmail.com>
2025-07-23 14:12:19 -0500

    initial commit

---
+++ scripts/Products.js
@@ -0,0 +1,18 @@
+import { getProducts } from "./database.js"
+
+const products = getProducts()
+
+export const Products = () => {
+    let html = "<ul>"
+    
+
+    for (const product of products) {
+ 
+        html += `<li>${product.name}</li>`
+    }
+
+    html += "</ul>"
+
+    return html
+}
+
