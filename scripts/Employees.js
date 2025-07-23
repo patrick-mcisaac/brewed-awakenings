6f1df259a185695fd2c87a0500959ffa6dc1aa67
patrick <patrick.mcisaac.dev@gmail.com>
2025-07-23 14:49:45 -0500

    window alert how many they sold

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,4 +1,4 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
 
@@ -6,7 +6,11 @@ export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `<li
+                data-type="employee"
+                data-employee-id="${employee.id}"
+                data-name="${employee.name}"
+                >${employee.name}</li>`
     }
 
     html += "</ul>"
@@ -14,3 +18,38 @@ export const Employees = () => {
     return html
 }
 
+document.addEventListener(
+    'click', (e) => {
+        // get what was clicked on
+        const wasCLicked = e.target
+
+        // check if it is type employee
+        let count = 0
+        if(wasCLicked.dataset.type === 'employee'){
+
+            // get employee id
+            const id = parseInt(wasCLicked.dataset.employeeId)
+    
+            // get orders
+            const orders = getOrders()
+    
+            // set a count variable to 0
+    
+            // iterate orders
+            for (const order of orders) {
+                
+                // check if employee id matches from order employee id
+                if(order.employeeId === id){
+                    // if so add one to the count
+                    count++
+                }
+    
+            }
+            
+        }
+        //  add a window alert
+        count === 1?
+        window.alert(`${wasCLicked.dataset.name} sold ${count} product`):
+        window.alert(`${wasCLicked.dataset.name} sold ${count} products`)
+    }
+)
